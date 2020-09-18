51713f47cb51cdbc69e85a15f5bc8f4e4dccedda
Edvin Gavell <eddi@live.se>
2020-09-18 19:00:04 +0200

    Added txt files to be able to run the Program. Added code to Main.java (the program code).

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void stepThroughFileAndSearch(File startFolder, String searchWord){
+        if (startFolder.isDirectory()){
+            File[] folderContents = startFolder.listFiles();
+            assert folderContents != null;
+            for (File f : folderContents) {
+                stepThroughFileAndSearch(f, searchWord);
+            }
+        } else if (startFolder.isFile()){
+            try {
+                Scanner sc = new Scanner(startFolder);
+                String s;
+                while(sc.hasNextLine()) {
+                    s = sc.nextLine();
+                    if (s.contains(searchWord)){
+                        System.out.println(startFolder.getName() + " " + startFolder.getAbsolutePath());
+                    }
+                }
+                sc.close();
+            } catch (Exception e) {
+                System.err.println("An error occured.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        File startingFolder = new File("txtfiler/");
+
+        System.out.println("-------Program FileCrawler-------");
+        System.out.print("Vad vill du söka efter? ");
+        String sokOrd = sc.next();
+        System.out.println("Du söker efter: " + sokOrd + " \ni mapp: " + startingFolder);
+        System.out.println();
+
+        stepThroughFileAndSearch(startingFolder, sokOrd);
+    }
+}
+
